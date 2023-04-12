ce88ca8a72061e9a474c4939ade8ee01336e76cb
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-12 22:38:20 +0700

    create: Header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex h-16 items-center justify-between border-b border-gray-200 bg-white px-4">
+      <div>left content</div>
+
+      <div>right content</div>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 // import components
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
     <main className="flex h-screen w-screen flex-row overflow-hidden bg-white">
       <Sidebar />
-      {children}
+      <div className="flex flex-1 flex-col">
+        <Header />
+        <div className="min-h-0 flex-1 overflow-auto p-4">{children}</div>
+      </div>
     </main>
   );
 };
