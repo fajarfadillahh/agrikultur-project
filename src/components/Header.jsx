9e57c6ae1d37f7d3f37306637c987ea7d1544d51
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-12 22:49:26 +0700

    modified: add search bar at the Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { RiSearchLine } from "react-icons/ri";
 
 const Header = () => {
   return (
     <div className="flex h-16 items-center justify-between border-b border-gray-200 bg-white px-4">
-      <div>left content</div>
+      <div className="inline-flex h-[42px] w-[300px] items-center gap-2 rounded-md border border-gray-200 px-4 text-gray-600">
+        <RiSearchLine />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="h-full w-full text-gray-900 outline-none"
+        />
+      </div>
 
       <div>right content</div>
     </div>
