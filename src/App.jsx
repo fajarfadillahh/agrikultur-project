697e236342826c3bf2a9b46c0ec0950287be44d8
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-13 22:14:39 +0700

    create: Dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import pages
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 // import components
 import Layout from "./components/Layout";
@@ -12,7 +13,9 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 // import components
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <main className="flex h-screen w-screen flex-row overflow-hidden bg-white">
       <Sidebar />
       <div className="flex flex-1 flex-col">
         <Header />
-        <div className="min-h-0 flex-1 overflow-auto p-4">{children}</div>
+        <div className="min-h-0 flex-1 overflow-auto p-4">
+          <Outlet />
+        </div>
       </div>
     </main>
   );
