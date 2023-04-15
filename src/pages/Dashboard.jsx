c9d9fb4cab7cc27a529df61a63d798ccff5e9062
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-15 13:22:22 +0700

    modified: add table user at the Dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,64 @@
 import React from "react";
+import { RiSearchLine } from "react-icons/ri";
+
+// import data
+import { user_data } from "../constants/UserData";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-[24px] font-semibold text-gray-900">Dashboard</h1>
+    <div className="flex flex-col gap-8">
+      <h1 className="section-title">Dashboard</h1>
+
+      <div className="grid gap-4">
+        <div className="inline-flex h-[42px] w-[300px] items-center gap-2 rounded-md border border-gray-200 px-4 text-gray-600">
+          <RiSearchLine />
+          <input
+            type="text"
+            placeholder="Search user"
+            className="h-full w-full text-gray-900 outline-none"
+          />
+        </div>
+
+        <table className="table-fixed border border-gray-200">
+          <thead>
+            <tr className="divide-x bg-gray-100 text-left">
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 font-semibold text-gray-600">
+                Registration Number
+              </th>
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 font-semibold text-gray-600">
+                User Name
+              </th>
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 font-semibold text-gray-600">
+                Address
+              </th>
+              <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 font-semibold text-gray-600">
+                Email
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {user_data.map((user) => {
+              return (
+                <tr key={user.id} className="divide-x bg-white even:bg-gray-50">
+                  <td className="border-b border-gray-200 px-5 py-5">
+                    {user.nik}
+                  </td>
+                  <td className="border-b border-gray-200 px-5 py-5">
+                    {user.name}
+                  </td>
+                  <td className="border-b border-gray-200 px-5 py-5">
+                    {user.address}
+                  </td>
+                  <td className="border-b border-gray-200 px-5 py-5">
+                    {user.email}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
